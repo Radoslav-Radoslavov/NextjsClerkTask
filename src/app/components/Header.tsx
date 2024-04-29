91dcee589f5ae93dae2554f033a331e54a6248c1
Radoslav Radoslavov <radoslav.radoslavov.prime@gmail.com>
2024-04-29 12:43:14 +0300

    updated home page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,11 +6,11 @@ const Header = () => {
   const { userId } = auth();
 
   return (
-    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-black w-full">
+    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-gray-600 w-full">
       <div className="flex items-center">
         <Link href={`${userId ? '/dashboard' : '/'}`}>
-          <div className="text-lg font-bold text-white uppercase">
-            Clerk auth
+          <div className="text-xl font-semibold text-white uppercase">
+            Clerk Authentication
           </div>
         </Link>
       </div>
@@ -18,32 +18,20 @@ const Header = () => {
       <div className="flex items-center text-white">
         {!userId && (
           <>
-            <Link
-              href="sign-in"
-              className="text-gray-300 hover:text-white mr-4"
-            >
+            <Link href="sign-in" className="text-gray-300 hover:text-white mr-4 text-sm font-medium">
               Sign In
             </Link>
-            <Link
-              href="sign-up"
-              className="text-gray-300 hover:text-white mr-4"
-            >
+            <Link href="sign-up" className="text-gray-300 hover:text-white mr-4 text-sm font-medium">
               Sign Up
             </Link>
           </>
         )}
         {userId && (
           <>
-            <Link
-              href="dashboard"
-              className="text-gray-300 hover:text-white mr-4"
-            >
+            <Link href="dashboard" className="text-gray-300 hover:text-white mr-4 text-sm font-medium">
               Dashboard
             </Link>
-            <Link
-              href="profile"
-              className="text-gray-300 hover:text-white mr-4"
-            >
+            <Link href="profile" className="text-gray-300 hover:text-white mr-4 text-sm font-medium">
               Profile
             </Link>
             <div className="ml-auto">
