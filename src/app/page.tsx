ae46c72cf483251aeb4c1fe07f89b17d9b7be318
Radoslav Radoslavov <radoslav.radoslavov.prime@gmail.com>
2024-04-29 13:04:41 +0300

    fixed home page name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold text-center mt-10">
-        Welcome to ClerkLogin
+        Welcome to Clerk Login
       </h1>
       <p className="mt-10 text-center">
         This is the example usage of Clerk authentication.
